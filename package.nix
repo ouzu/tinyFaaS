80290449e4285e9c7b58a7be18883dc05df82b96
ouzu <git@ouzu.tech>
2023-09-15 22:25:47 +0200

    include runtimes in nix package

--- package.nix
+++ package.nix
@@ -4,4 +4,8 @@ buildGoModule {
   name = "tinyFaaS";
   src = lib.cleanSource ./.;
   vendorSha256 = "sha256-Mqr8OpxxJ7q0V16+a+PzZotPjh5XJyyCNyYHkmER0ck=";
+  postBuild = ''
+    mkdir -p $out/share/tinyFaaS
+    cp -r $src/runtimes $out/share/tinyFaaS
+  '';
 }
